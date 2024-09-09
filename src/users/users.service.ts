af7abb7399eecdd937dc91aabc8615517f5d26fe
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-09-09 17:18:15 -0500

    modificando user schema

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schema/user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
+}
