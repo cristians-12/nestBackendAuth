68c497a3ae70f167c8aef86174b567595d8bf71c
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-09-10 17:18:59 -0500

    add domain

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:3000',
       'https://c263hlnk-3000.use.devtunnels.ms',
+      'https://movie-test-chi.vercel.app',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
